1b7e8316e34f6e150ebd82fc72303d7009530186
Stephen Leitnick <sleitnick@gmail.com>
2022-07-27 13:39:37 -0400

    Tabs

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,23 @@ use cli::Cli;
 
 #[tokio::main]
 async fn main() {
-    env_logger::builder()
-        .format_timestamp(None)
-        .filter_level(log::LevelFilter::Info)
-        .init();
+	env_logger::builder()
+		.format_timestamp(None)
+		.filter_level(log::LevelFilter::Info)
+		.init();
 
-    let cli_args = Cli::parse();
+	let cli_args = Cli::parse();
 
-    match cli_args.run().await {
-        Ok(str) => {
-            if let Some(s) = str {
-                // log::info!("{}", s);
-                println!("{}", s);
-            }
-        }
-        Err(err) => {
-            log::error!("{:?}", err);
-            process::exit(1);
-        }
-    }
+	match cli_args.run().await {
+		Ok(str) => {
+			if let Some(s) = str {
+				// log::info!("{}", s);
+				println!("{}", s);
+			}
+		}
+		Err(err) => {
+			log::error!("{:?}", err);
+			process::exit(1);
+		}
+	}
 }
